7224738ed9e36976c1fb5cb3fed4c7f1797f1b61
Sanqui <gsanky@gmail.com>
2016-03-28 12:45:17 +0200

    Build with make, depending on pret as submodule

--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ve
-PROJECT="telefang"
-rgbasm -E -o $PROJECT.o main.asm
-rgblink -n $PROJECT.sym -m $PROJECT.map -O baserom.gbc -o $PROJECT.gbc $PROJECT.o
-rgbfix -v -c -i BXTJ -k 2N -l 0x33 -m 0x10 -p 0 -r 3 -t "TELEFANG PW" $PROJECT.gbc
-cmp baserom.gbc $PROJECT.gbc
