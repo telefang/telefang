6431b17fbcafe7b291e6bd4ea288eaf79cc3233e
Sanqui <gsanky@gmail.com>
2016-03-28 22:17:57 +0200

    That's what I get for trying to be clever (fix phone incbins)

--- main.asm
+++ main.asm
@@ -71,19 +71,44 @@ DenjuuStageGfx:
 	INCBIN "gfx/denjuu_stages.2bpp"
 
 SECTION "Phone keypad graphics", ROMX[$4000], BANK[$79]
-phone_keypad_gfx: MACRO
-	INCBIN "gfx/phones/\1/numbers.2bpp"
-	INCBIN "gfx/phones/\1/menus.2bpp"
-	INCBIN "gfx/phones/\1/letters1.2bpp"
-	INCBIN "gfx/phones/\1/menus.2bpp"
-	INCBIN "gfx/phones/\1/letters2.2bpp"
-	INCBIN "gfx/phones/\1/menus.2bpp"
-	INCBIN "gfx/phones/\1/solfege.2bpp"
-	INCBIN "gfx/phones/\1/menus.2bpp"
-ENDM
+; XXX no rgbdep yet...
+;phone_keypad_gfx: MACRO
+;	INCBIN "gfx/phones/\1/numbers.2bpp"
+;	INCBIN "gfx/phones/\1/menus.2bpp"
+;	INCBIN "gfx/phones/\1/letters1.2bpp"
+;	INCBIN "gfx/phones/\1/menus.2bpp"
+;	INCBIN "gfx/phones/\1/letters2.2bpp"
+;	INCBIN "gfx/phones/\1/menus.2bpp"
+;	INCBIN "gfx/phones/\1/solfege.2bpp"
+;	INCBIN "gfx/phones/\1/menus.2bpp"
+;ENDM
+
 
 PhoneKeypadGfx:
-    phone_keypad_gfx 0
-    phone_keypad_gfx 1
-    phone_keypad_gfx 2
+	INCBIN "gfx/phones/0/numbers.2bpp"
+	INCBIN "gfx/phones/0/menus.2bpp"
+	INCBIN "gfx/phones/0/letters1.2bpp"
+	INCBIN "gfx/phones/0/menus.2bpp"
+	INCBIN "gfx/phones/0/letters2.2bpp"
+	INCBIN "gfx/phones/0/menus.2bpp"
+	INCBIN "gfx/phones/0/solfege.2bpp"
+	INCBIN "gfx/phones/0/menus.2bpp"
+	
+	INCBIN "gfx/phones/1/numbers.2bpp"
+	INCBIN "gfx/phones/1/menus.2bpp"
+	INCBIN "gfx/phones/1/letters1.2bpp"
+	INCBIN "gfx/phones/1/menus.2bpp"
+	INCBIN "gfx/phones/1/letters2.2bpp"
+	INCBIN "gfx/phones/1/menus.2bpp"
+	INCBIN "gfx/phones/1/solfege.2bpp"
+	INCBIN "gfx/phones/1/menus.2bpp"
+	
+	INCBIN "gfx/phones/2/numbers.2bpp"
+	INCBIN "gfx/phones/2/menus.2bpp"
+	INCBIN "gfx/phones/2/letters1.2bpp"
+	INCBIN "gfx/phones/2/menus.2bpp"
+	INCBIN "gfx/phones/2/letters2.2bpp"
+	INCBIN "gfx/phones/2/menus.2bpp"
+	INCBIN "gfx/phones/2/solfege.2bpp"
+	INCBIN "gfx/phones/2/menus.2bpp"
 
