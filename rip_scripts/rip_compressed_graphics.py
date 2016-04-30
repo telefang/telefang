4f6490dd719a5f9f88580bf4010b714162540e28
David Wendt <dcrkid@yahoo.com>
2016-04-29 21:04:07 -0400

    Add support for a different compressed_gfx_names masterlist. The compressed graphics names now live with their version data.

--- rip_scripts/rip_compressed_graphics.py
+++ rip_scripts/rip_compressed_graphics.py
@@ -90,7 +90,7 @@ for i in range(NUMGFX):
 
 
 i = 0
-for line in open("rip_scripts/compressed_graphic_names.txt"):
+for line in open(sys.argv[2] + "/compressed_gfx_names.txt"):
     if " " in line:
         i, filename = line.strip().split(" ")
         i = int(i, 16)
@@ -135,25 +135,25 @@ for i in range(NUMGFX):
             print '\tdb COMPRESSED'
         else:
             print '\tdbw NOT_COMPRESSED, {}End - {} - 3'.format(g['label'], g['label'])
-        print '\tINCBIN "' + sys.argv[2] + '/{}.{}"'.format(g['filename'], ['2bpp', 'malias'][g['is_compressed']])
+        print '\tINCBIN "' + sys.argv[2] + '/gfx/{}.{}"'.format(g['filename'], ['2bpp', 'malias'][g['is_compressed']])
         print "{}End".format(g['label'])
 
 for i in range(NUMGFX):
     g = graphics[i]
     if g['filename']:
-        path = sys.argv[2] + '/{}'.format('/'.join(g['filename'].split('/')[:-1]))
+        path = sys.argv[2] + '/gfx/{}'.format('/'.join(g['filename'].split('/')[:-1]))
         if not os.path.isdir(path):
             os.mkdir(path)
         if g['is_compressed']:
-            open(sys.argv[2] + "/{}.malias".format(g['filename']), "wb").write(g['data'])
-        open(sys.argv[2] + "/{}.2bpp".format(g['filename']), "wb").write(g['decompressed_data'])
-        os.system("python pokemon-reverse-engineering-tools/pokemontools/gfx.py png " + sys.argv[2] + "/{}.2bpp".format(g['filename']))
+            open(sys.argv[2] + "/gfx/{}.malias".format(g['filename']), "wb").write(g['data'])
+        open(sys.argv[2] + "/gfx/{}.2bpp".format(g['filename']), "wb").write(g['decompressed_data'])
+        os.system("python pokemon-reverse-engineering-tools/pokemontools/gfx.py png " + sys.argv[2] + "/gfx/{}.2bpp".format(g['filename']))
         
         if g['is_compressed']:
-            os.system("git add " + sys.argv[2] + "/{}.malias".format(g['filename']))
+            os.system("git add " + sys.argv[2] + "/gfx/{}.malias".format(g['filename']))
         else:
-            os.system("git add " + sys.argv[2] + "/{}.2bpp".format(g['filename']))
-        os.system("git add " + sys.argv[2] + "/{}.png".format(g['filename']))
+            os.system("git add " + sys.argv[2] + "/gfx/{}.2bpp".format(g['filename']))
+        os.system("git add " + sys.argv[2] + "/gfx/{}.png".format(g['filename']))
             
 
 #for i in range(NUMGFX):
