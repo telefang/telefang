25797d115c35b76ea759b184834d559b20005316
David Wendt <dcrkid@yahoo.com>
2016-05-09 02:26:47 -0400

    TODO: Disassemble bank 1 code.

--- components/system/empty.asm
+++ components/system/empty.asm
@@ -1,4 +1,3 @@
-;For some reason, all of Bank 1 is unused.
-;It makes it easier to not have to disassemble bank 1 crap in IDA :P
+;Remember that comment I made in the master branch? HAHAHAHAHAHAHAHAHAHAHA
 SECTION "bank 1", ROMX[$4000], BANK[$1]
-    ds $4000 ; bank 1 is empty
+    ;Bank 1 is NOT empty in patch.
