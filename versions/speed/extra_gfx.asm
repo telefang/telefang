dcddb6c66c371c12bbc47065d97e7970054924a9
David Wendt <dcrkid@yahoo.com>
2016-10-12 00:13:19 -0400

    Add the graphics for the signal levels

--- versions/speed/extra_gfx.asm
+++ versions/speed/extra_gfx.asm
@@ -9,3 +9,8 @@ SECTION "gfx/menu/script_window.2bpp", ROMX[$719C], BANK[$38]
 ScriptWindow::
 	INCBIN "gfx/menu/script_window.2bpp"
 ScriptWindow_END::
+	
+SECTION "gfx/menu/signal_levels.2bpp", ROMX[$44D8], BANK[$38]
+Menu_SignalLevelsGfx::
+	INCBIN "gfx/menu/signal_levels.2bpp"
+Menu_SignalLevelsGfx_END::
