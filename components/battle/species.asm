bdbec79476178fd27b8416f15dbc62a7dc60e194
David Wendt <dcrkid@yahoo.com>
2017-02-13 01:38:03 -0500

    Add disassembly of the function that loads the EXP curve for a particular Denjuu's type.

--- components/battle/species.asm
+++ components/battle/species.asm
@@ -1,9 +1,17 @@
 INCLUDE "components/battle/species.inc"
+INCLUDE "components/saveclock/save_format.inc"
+
+SECTION "Battle Denjuu Species Info WRAM", WRAMX[$D45F], BANK[1]
+W_Battle_RetrSpeciesByte:: ds 1
+
+SECTION "Battle Denjuu Species Info WRAM 1", WRAMX[$D494], BANK[1]
+W_Battle_RetrLevelupByte:: ds 1
 
 ;TODO: Symbolize the addresses needed to load this data.
 SECTION "Battle Denjuu Species Info Functions", ROM0[$3A35]
 ;Load a byte from the species-wide data at $1D4B48.
 ; A = Species (clobbers W_Status_SelectedDenjuuSpecies)
+; B = Denjuu Level
 ; C = Data index (which byte you want back)
 ; Returns retrieved value in W_Battle_RetrSpeciesByte
 Battle_LoadSpeciesData::
@@ -27,16 +35,16 @@ Battle_LoadSpeciesData::
     ld e, a
     add hl, de
     ld a, [hl]
-    ld [W_Battle_RetrSpeciesByte]
+    ld [W_Battle_RetrSpeciesByte], a
     
     ;Return value directly if it's not a base stat.
     ld a, c
     cp M_Battle_Move1
     ret nc
     
-    ;Return value directly if B < 2...
-    ;TODO: What purpose does this serve? All known callers of the banksafe
-    ;wrapper don't set B. Sounds like a bug factory.
+    ;Return value directly if Denjuu is LV1.
+    ;Otherwise, scale the base stat by the given level, using the species' stat
+    ;scaling table.
     ld a, b
     cp 2
     ret c
@@ -76,6 +84,41 @@ Battle_LoadSpeciesData::
     add a, e
     ld [W_Battle_RetrSpeciesByte], a
     ret
+    
+SECTION "Battle Denjuu Species EXP Functions", ROM0[$3B4E]
+; Determine the EXP requirement for the next level.
+; 
+; A = Species type
+; B = Level
+; 
+; Each of the six types gets it's own EXP curve table.
+; 
+; Expects to be run with ROM27 mapped in. Must be called from a banksafe
+; function.
+Battle_LoadNextLevelExp::
+    ld hl, $4000
+    ld de, M_SaveClock_DenjuuLevelCap * 2
+    cp 0
+    jr z, .noMul
+    
+.mulLoop
+    add hl, de
+    dec a
+    jr nz, .mulLoop
+    
+.noMul
+    dec b
+    ld d, 0
+    ld a, b
+    ld e, a
+    sla e
+    rl d
+    add hl, de
+    ld c, [hl]
+    inc hl
+    ld a, [hl]
+    ld b, a
+    ret
 
 SECTION "Battle Denjuu Species Levelup Info Functions", ROM0[$3BA9]
 ;Read the per-levelup scale factor for a particular denjuu and level.
@@ -88,7 +131,7 @@ SECTION "Battle Denjuu Species Levelup Info Functions", ROM0[$3BA9]
 ;
 ;Essentially: return (*0x4715)[A][B];
 ;Intended to be called from a banksafe function that enters ROM27.
-Battle_LoadLevelupData:
+Battle_LoadLevelupData::
     ld hl, $4715
     ld de, 6
     cp 0
