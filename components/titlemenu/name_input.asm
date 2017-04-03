3f044b18664fc9cdfe3b47b2642564399910bba9
David Wendt <dcrkid@yahoo.com>
2017-04-03 00:37:26 -0400

    Here's the code that positions cursors for the player naming and denjuu nicknaming screens on the Title Menu

---
+++ components/titlemenu/name_input.asm
@@ -0,0 +1,45 @@
+SECTION "Title Menu Player Name Input", ROMX[$68FF], BANK[$4]
+TitleMenu_PositionNameCursor::
+    ld a, [W_PauseMenu_SelectedMenuItem]
+    ld e, a
+    ld d, 0
+    ld hl, .cursorPositionList
+    add hl, de
+    ld a, [hl]
+    
+    ld b, a
+    ld c, $68
+    ld de, $C0C0 ;TODO: This is the cursor structure
+    call PauseMenu_PositionCursor
+    ld a, 1
+    ld [W_OAM_SpritesReady], a
+    ret
+    
+.cursorPositionList
+    db $08
+    db $10
+    db $18
+    db $20
+    db $28
+    db $30
+    db $38
+    db $40
+    
+;Nearly identical function to the above...
+;It doesn't use a lookup table, but instead calculates the X coordinate in the
+;same way, all for an 8 pixel offset...
+;Anyway, it's called by state 03 20, which I -think- has something to do with
+;nicknaming denjuu obtained through link communications.
+TitleMenu_PositionNicknameCursor::
+    ld a, [W_PauseMenu_SelectedMenuItem]
+    sla a
+    sla a
+    sla a
+    add a, $10
+    ld b, a
+    ld c, $70
+    ld de, $C0C0 ;TODO: Cursor structure
+    call PauseMenu_PositionCursor
+    ld a, 1
+    ld [W_OAM_SpritesReady], a
+    ret
