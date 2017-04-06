d0f280a46ffae03d2088ef6078f0daf46502478e
David Wendt <dcrkid@yahoo.com>
2017-04-06 01:36:18 -0400

    We have too manu 'utility' files, so move this into the tilemaps file where it makes more sense

--- components/pausemenu/tilemaps.asm
+++ components/pausemenu/tilemaps.asm
@@ -1,3 +1,21 @@
+SECTION " Pause Menu Screen Utils", ROMX[$5A06], BANK[4]
+PauseMenu_ClearScreenTiles::
+    push bc
+    ld c, $10
+    
+.loop1
+    xor a
+    call YetAnotherWFB
+    ld [hli], a
+    dec c
+    jr nz, .loop1
+    
+    pop bc
+    dec b
+    jr nz, PauseMenu_ClearScreenTiles
+    
+    ret
+
 SECTION "Pause Menu Tilemap Loading", ROMX[$7931], BANK[$4]
 
 ; All of these are thunks/frontends into the tilemap decompressor.
