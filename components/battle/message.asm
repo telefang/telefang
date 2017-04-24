42ee1b20dc12f1375f0d9718a1a6dc7be828d6bc
David Wendt <dcrkid@yahoo.com>
2017-04-23 22:24:27 -0400

    This code handles intermediate staging and string retermination

--- components/battle/message.asm
+++ components/battle/message.asm
@@ -1,5 +1,11 @@
 INCLUDE "telefang.inc"
 
+SECTION "Battle Message Argument Memory", WRAMX[$D420], BANK[$1]
+W_Battle_TableStringStaging:: ds M_StringTable_Load8AreaSize
+
+SECTION "Battle Message Argument Memory 2", WRAMX[$D45A], BANK[$1]
+W_Battle_LoopIndex:: ds 1
+
 SECTION "Battle Message Argument Writers", ROMX[$4012], BANK[$5]
 Battle_SetMessageArg2Denjuu::
     ld de, W_MainScript_MessageArg2
@@ -18,3 +24,33 @@ Battle_SetMessageArg2Phrase::
 .memcpy
     ld b, 0
     jp memcpy
+
+;Used for intermediary staging before writing to the message arguments.
+;BC is the source address. Returns the string staging target as HL, perfect for
+;then immediately calling SetMessageArgn functions.
+Battle_CopyTableString::
+    ld hl, W_Battle_TableStringStaging
+    ld a, M_StringTable_Load8AreaSize
+    ld [W_Battle_LoopIndex], a
+    
+.loop
+    ld a, [bc]
+    cp $C0
+    jr z, .terminate
+    
+    ld [hl], a
+    inc hl
+    inc bc
+    
+    ld a, [W_Battle_LoopIndex]
+    dec a
+    ld [W_Battle_LoopIndex], a
+    
+    jr nz, .loop
+    
+.terminate
+    ld a, $E0
+    ld [hl], a
+    
+    ld hl, W_Battle_TableStringStaging
+    ret
