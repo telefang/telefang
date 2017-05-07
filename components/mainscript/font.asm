15181f439e131a883e890b54e66fc7aeb3a908f1
David Wendt <dcrkid@yahoo.com>
2017-05-06 22:56:51 -0400

    Account for this odd thing...

--- components/mainscript/font.asm
+++ components/mainscript/font.asm
@@ -1,5 +1,8 @@
 INCLUDE "components/mainscript/font.inc"
 
-SECTION "Main Script Font", ROMX[$5229], BANK[$B]
+SECTION "Main Script Font", ROMX[$5228], BANK[$B]
+;This is probably an image injection mistake, but include it anyway.
+;TODO: What happens if we remove this byte?
+    db 0
 MainScript_Font::
     INCBIN "gfx/font.1bpp", 0, NUM_CHARACTERS * 8
