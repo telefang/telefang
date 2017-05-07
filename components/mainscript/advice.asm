dfa07029272e8639862be2ee0cc7cb24b61f8f6b
David Wendt <dcrkid@yahoo.com>
2017-05-06 23:54:46 -0400

    Here, have more trash bytes - I think... I can't get any of this code to trigger yet, nor will access breakpoints on the second bit of trash ever execute...

--- components/mainscript/advice.asm
+++ components/mainscript/advice.asm
@@ -8,3 +8,22 @@ W_MainScript_VWFMainScriptHack:: ds 1
 W_MainScript_VWFDisable:: ds 1
 
 ;Actual patches exist in their own files due to RGBDS limitations.
+;Here have some trashbytes instead
+
+SECTION "MainScript ADVICE Trash Bytes", ROMX[$79BC], BANK[$B]
+;This points to somewhere inside of StoreCurrentLetter and I don't know why.
+    jp $7C12
+    
+SECTION "MainScript ADVICE Endless Trash", ROMX[$7CD7], BANK[$B]
+;I have no idea and bgb's access breakpoints never trip on these bytes.
+    REPT 9
+    db $FF
+    ENDR
+    
+    REPT 7
+    db $00
+    ENDR
+    
+    REPT 21
+    db $FF
+    ENDR
