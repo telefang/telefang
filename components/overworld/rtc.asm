ee67e204448d339eb27bd3bcb874155ce5643566
David Wendt <dcrkid@yahoo.com>
2017-05-12 02:00:46 -0400

    Add advisory code to fix bad inaccuracies in Visual Boy Advance.
    THIS COMMIT IS A CANDIDATE FOR BACKPORTING TO THE JAPANESE VERSION.

--- components/overworld/rtc.asm
+++ components/overworld/rtc.asm
@@ -31,14 +31,20 @@ Overworld_ReadRTCTime::
     nop
     nop
     ld a, [$A000]
-    ld [W_Overworld_CurrentTimeMinutes], a
+    
+    push bc
+    push af
     
     ld a, $A
     ld [REG_MBC3_SRAMBANK], a
     nop
     nop
-    ld a, [$A000]
+    jp Overworld_ADVICE_ReadRTCTime
+    
+.comefromAdvice
     ld [W_Overworld_CurrentTimeHours], a
+    
+    pop bc ;Not unbalanced, advice code pops once.
     
     ld a, $B
     ld [REG_MBC3_SRAMBANK], a
@@ -58,3 +64,30 @@ Overworld_ReadRTCTime::
     ld a, 0
     ld [REG_MBC3_SRAMENABLE], a
     ret
+    
+SECTION "Overworld RTC Advice", ROMX[$7F9D], BANK[$29]
+;Emulator-compatibility fix for Visual Boy Advance, whose emulated RTC counts
+;from 1-60 instead of 0-59. Notably this breaks palette loading, causing the
+;entire game to white out at 2400-2460 hours (a nonexistent time)
+Overworld_ADVICE_ReadRTCTime::
+    ld a, [$A000] ;Expected pointcut is right after the hours have been latched
+    ld b, a
+    
+    pop af ;These are minutes we pushed before the pointcut
+    
+.minutesBound
+    cp 60
+    jp c, .writeFixedMinutes
+    sub 60
+    inc b
+    jp .minutesBound
+    
+.writeFixedMinutes
+    ld [W_Overworld_CurrentTimeMinutes], a
+    ld a, b
+    
+.hoursBound
+    cp 24
+    jp c, Overworld_ReadRTCTime.comefromAdvice
+    sub 24
+    jp .hoursBound
