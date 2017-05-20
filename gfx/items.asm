78e8f25b38fd13c479494863ca0daf87e125f8ef
David Wendt <dcrkid@yahoo.com>
2017-05-20 16:10:55 -0400

    Translate "No Items"

---
+++ gfx/items.asm
@@ -0,0 +1,204 @@
+
+SECTION "gfx/items/0/0.w48.2bpp", ROMX[$4000], BANK[$2b]
+	INCBIN "gfx/items/0/0.w48.2bpp"
+
+SECTION "gfx/items/0/1.w48.2bpp", ROMX[$41e0], BANK[$2b]
+	INCBIN "gfx/items/0/1.w48.2bpp"
+
+SECTION "gfx/items/0/2.w48.2bpp", ROMX[$43c0], BANK[$2b]
+	INCBIN "gfx/items/0/2.w48.2bpp"
+
+SECTION "gfx/items/0/3.w48.2bpp", ROMX[$45a0], BANK[$2b]
+	INCBIN "gfx/items/0/3.w48.2bpp"
+
+SECTION "gfx/items/0/4.w48.2bpp", ROMX[$4780], BANK[$2b]
+	INCBIN "gfx/items/0/4.w48.2bpp"
+
+SECTION "gfx/items/0/5.w48.2bpp", ROMX[$4960], BANK[$2b]
+	INCBIN "gfx/items/0/5.w48.2bpp"
+
+SECTION "gfx/items/0/6.w48.2bpp", ROMX[$4b40], BANK[$2b]
+	INCBIN "gfx/items/0/6.w48.2bpp"
+
+SECTION "gfx/items/0/7.w48.2bpp", ROMX[$4d20], BANK[$2b]
+	INCBIN "gfx/items/0/7.w48.2bpp"
+
+SECTION "gfx/items/0/8.w48.2bpp", ROMX[$4f00], BANK[$2b]
+	INCBIN "gfx/items/0/8.w48.2bpp"
+
+SECTION "gfx/items/0/9.w48.2bpp", ROMX[$50e0], BANK[$2b]
+	INCBIN "gfx/items/0/9.w48.2bpp"
+
+SECTION "gfx/items/0/10.w48.2bpp", ROMX[$52c0], BANK[$2b]
+	INCBIN "gfx/items/0/10.w48.2bpp"
+
+SECTION "gfx/items/0/11.w48.2bpp", ROMX[$54a0], BANK[$2b]
+	INCBIN "gfx/items/0/11.w48.2bpp"
+
+SECTION "gfx/items/0/12.w48.2bpp", ROMX[$5680], BANK[$2b]
+	INCBIN "gfx/items/0/12.w48.2bpp"
+
+SECTION "gfx/items/0/13.w48.2bpp", ROMX[$5860], BANK[$2b]
+	INCBIN "gfx/items/0/13.w48.2bpp"
+
+SECTION "gfx/items/0/14.w48.2bpp", ROMX[$5a40], BANK[$2b]
+	INCBIN "gfx/items/0/14.w48.2bpp"
+
+SECTION "gfx/items/0/15.w48.2bpp", ROMX[$5c20], BANK[$2b]
+	INCBIN "gfx/items/0/15.w48.2bpp"
+
+SECTION "gfx/items/0/16.w48.2bpp", ROMX[$5e00], BANK[$2b]
+	INCBIN "gfx/items/0/16.w48.2bpp"
+
+SECTION "gfx/items/0/17.w48.2bpp", ROMX[$5fe0], BANK[$2b]
+	INCBIN "gfx/items/0/17.w48.2bpp"
+
+SECTION "gfx/items/0/18.w48.2bpp", ROMX[$61c0], BANK[$2b]
+	INCBIN "gfx/items/0/18.w48.2bpp"
+
+SECTION "gfx/items/0/19.w48.2bpp", ROMX[$63a0], BANK[$2b]
+	INCBIN "gfx/items/0/19.w48.2bpp"
+
+SECTION "gfx/items/0/20.w48.2bpp", ROMX[$6580], BANK[$2b]
+	INCBIN "gfx/items/0/20.w48.2bpp"
+
+SECTION "gfx/items/0/21.w48.2bpp", ROMX[$6760], BANK[$2b]
+	INCBIN "gfx/items/0/21.w48.2bpp"
+
+SECTION "gfx/items/0/22.w48.2bpp", ROMX[$6940], BANK[$2b]
+	INCBIN "gfx/items/0/22.w48.2bpp"
+
+SECTION "gfx/items/0/23.w48.2bpp", ROMX[$6b20], BANK[$2b]
+	INCBIN "gfx/items/0/23.w48.2bpp"
+
+SECTION "gfx/items/0/24.w48.2bpp", ROMX[$6d00], BANK[$2b]
+	INCBIN "gfx/items/0/24.w48.2bpp"
+
+SECTION "gfx/items/0/25.w48.2bpp", ROMX[$6ee0], BANK[$2b]
+	INCBIN "gfx/items/0/25.w48.2bpp"
+
+SECTION "gfx/items/0/26.w48.2bpp", ROMX[$70c0], BANK[$2b]
+	INCBIN "gfx/items/0/26.w48.2bpp"
+
+SECTION "gfx/items/0/27.w48.2bpp", ROMX[$72a0], BANK[$2b]
+	INCBIN "gfx/items/0/27.w48.2bpp"
+
+SECTION "gfx/items/0/28.w48.2bpp", ROMX[$7480], BANK[$2b]
+	INCBIN "gfx/items/0/28.w48.2bpp"
+
+SECTION "gfx/items/0/29.w48.2bpp", ROMX[$7660], BANK[$2b]
+	INCBIN "gfx/items/0/29.w48.2bpp"
+
+SECTION "gfx/items/0/30.w48.2bpp", ROMX[$7840], BANK[$2b]
+	INCBIN "gfx/items/0/30.w48.2bpp"
+
+SECTION "gfx/items/0/31.w48.2bpp", ROMX[$7a20], BANK[$2b]
+	INCBIN "gfx/items/0/31.w48.2bpp"
+
+SECTION "gfx/items/0/32.w48.2bpp", ROMX[$7c00], BANK[$2b]
+	INCBIN "gfx/items/0/32.w48.2bpp"
+
+SECTION "gfx/items/0/33.w48.2bpp", ROMX[$7de0], BANK[$2b]
+	INCBIN "gfx/items/0/33.w48.2bpp"
+
+SECTION "gfx/items/1/0.w48.2bpp", ROMX[$4000], BANK[$2c]
+	INCBIN "gfx/items/1/0.w48.2bpp"
+
+SECTION "gfx/items/1/1.w48.2bpp", ROMX[$41e0], BANK[$2c]
+	INCBIN "gfx/items/1/1.w48.2bpp"
+
+SECTION "gfx/items/1/2.w48.2bpp", ROMX[$43c0], BANK[$2c]
+	INCBIN "gfx/items/1/2.w48.2bpp"
+
+SECTION "gfx/items/1/3.w48.2bpp", ROMX[$45a0], BANK[$2c]
+	INCBIN "gfx/items/1/3.w48.2bpp"
+
+SECTION "gfx/items/1/4.w48.2bpp", ROMX[$4780], BANK[$2c]
+	INCBIN "gfx/items/1/4.w48.2bpp"
+
+SECTION "gfx/items/1/5.w48.2bpp", ROMX[$4960], BANK[$2c]
+	INCBIN "gfx/items/1/5.w48.2bpp"
+
+SECTION "gfx/items/1/6.w48.2bpp", ROMX[$4b40], BANK[$2c]
+	INCBIN "gfx/items/1/6.w48.2bpp"
+
+SECTION "gfx/items/1/7.w48.2bpp", ROMX[$4d20], BANK[$2c]
+	INCBIN "gfx/items/1/7.w48.2bpp"
+
+SECTION "gfx/items/1/8.w48.2bpp", ROMX[$4f00], BANK[$2c]
+	INCBIN "gfx/items/1/8.w48.2bpp"
+
+SECTION "gfx/items/1/9.w48.2bpp", ROMX[$50e0], BANK[$2c]
+	INCBIN "gfx/items/1/9.w48.2bpp"
+
+SECTION "gfx/items/1/10.w48.2bpp", ROMX[$52c0], BANK[$2c]
+	INCBIN "gfx/items/1/10.w48.2bpp"
+
+SECTION "gfx/items/1/11.w48.2bpp", ROMX[$54a0], BANK[$2c]
+	INCBIN "gfx/items/1/11.w48.2bpp"
+
+SECTION "gfx/items/1/12.w48.2bpp", ROMX[$5680], BANK[$2c]
+	INCBIN "gfx/items/1/12.w48.2bpp"
+
+SECTION "gfx/items/1/13.w48.2bpp", ROMX[$5860], BANK[$2c]
+	INCBIN "gfx/items/1/13.w48.2bpp"
+
+SECTION "gfx/items/1/14.w48.2bpp", ROMX[$5a40], BANK[$2c]
+	INCBIN "gfx/items/1/14.w48.2bpp"
+
+SECTION "gfx/items/1/15.w48.2bpp", ROMX[$5c20], BANK[$2c]
+	INCBIN "gfx/items/1/15.w48.2bpp"
+
+SECTION "gfx/items/1/16.w48.2bpp", ROMX[$5e00], BANK[$2c]
+	INCBIN "gfx/items/1/16.w48.2bpp"
+
+SECTION "gfx/items/1/17.w48.2bpp", ROMX[$5fe0], BANK[$2c]
+	INCBIN "gfx/items/1/17.w48.2bpp"
+
+SECTION "gfx/items/1/18.w48.2bpp", ROMX[$61c0], BANK[$2c]
+	INCBIN "gfx/items/1/18.w48.2bpp"
+
+SECTION "gfx/items/1/19.w48.2bpp", ROMX[$63a0], BANK[$2c]
+	INCBIN "gfx/items/1/19.w48.2bpp"
+
+SECTION "gfx/items/1/20.w48.2bpp", ROMX[$6580], BANK[$2c]
+	INCBIN "gfx/items/1/20.w48.2bpp"
+
+SECTION "gfx/items/1/21.w48.2bpp", ROMX[$6760], BANK[$2c]
+	INCBIN "gfx/items/1/21.w48.2bpp"
+
+SECTION "gfx/items/1/22.w48.2bpp", ROMX[$6940], BANK[$2c]
+	INCBIN "gfx/items/1/22.w48.2bpp"
+
+SECTION "gfx/items/1/23.w48.2bpp", ROMX[$6b20], BANK[$2c]
+	INCBIN "gfx/items/1/23.w48.2bpp"
+
+SECTION "gfx/items/1/24.w48.2bpp", ROMX[$6d00], BANK[$2c]
+	INCBIN "gfx/items/1/24.w48.2bpp"
+
+SECTION "gfx/items/1/25.w48.2bpp", ROMX[$6ee0], BANK[$2c]
+	INCBIN "gfx/items/1/25.w48.2bpp"
+
+SECTION "gfx/items/1/26.w48.2bpp", ROMX[$70c0], BANK[$2c]
+	INCBIN "gfx/items/1/26.w48.2bpp"
+
+SECTION "gfx/items/1/27.w48.2bpp", ROMX[$72a0], BANK[$2c]
+	INCBIN "gfx/items/1/27.w48.2bpp"
+
+SECTION "gfx/items/1/28.w48.2bpp", ROMX[$7480], BANK[$2c]
+	INCBIN "gfx/items/1/28.w48.2bpp"
+
+SECTION "gfx/items/1/29.w48.2bpp", ROMX[$7660], BANK[$2c]
+	INCBIN "gfx/items/1/29.w48.2bpp"
+
+SECTION "gfx/items/1/30.w48.2bpp", ROMX[$7840], BANK[$2c]
+	INCBIN "gfx/items/1/30.w48.2bpp"
+
+SECTION "gfx/items/1/31.w48.2bpp", ROMX[$7a20], BANK[$2c]
+	INCBIN "gfx/items/1/31.w48.2bpp"
+
+SECTION "gfx/items/1/32.w48.2bpp", ROMX[$7c00], BANK[$2c]
+	INCBIN "gfx/items/1/32.w48.2bpp"
+
+SECTION "gfx/items/1/33.w48.2bpp", ROMX[$7de0], BANK[$2c]
+	INCBIN "gfx/items/1/33.w48.2bpp"
