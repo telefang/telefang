dc0f99d4a1d723a68a8dd396b47181891b9f6e57
David Wendt <dcrkid@yahoo.com>
2017-05-20 19:38:13 -0400

    These are trash

--- versions/power/extra_gfx.asm
+++ versions/power/extra_gfx.asm
@@ -14,3 +14,9 @@ SECTION "gfx/menu/battle_contact_select.2bpp", ROMX[$5A00], BANK[$42]
 BattleContactSelect::
 	INCBIN "gfx/menu/battle_contact_select.2bpp"
 BattleContactSelect_END::
+	REPT $83
+	db 0
+	ENDR
+	
+	;TODO: Remove these trashbytes
+	db 1, 0, 7, 0, $E, $F, $10, $10, $2F, $20, $1F, $46, $39
