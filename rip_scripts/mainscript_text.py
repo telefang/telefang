d775c6902f93890fe4b0edaa287a828ff6816097
David Wendt <dcrkid@yahoo.com>
2017-05-25 00:31:53 -0400

    stop hexing plz

--- rip_scripts/mainscript_text.py
+++ rip_scripts/mainscript_text.py
@@ -855,7 +855,7 @@ def main():
     ap.add_argument('--output', type=str, default="script")
     ap.add_argument('--metatable_loc', type=int, default=METATABLE_LOC)
     ap.add_argument('--metrics_loc', type=int, default=0x2FB00)
-    ap.add_argument('--window_width', type=int, default=0x16 * 0x8) #16 tiles
+    ap.add_argument('--window_width', type=int, default=16 * 8) #16 tiles
     ap.add_argument('--overflow_bank', type=int, default=0x1E)
     ap.add_argument('rom', type=str)
     ap.add_argument('filenames', type=str, nargs="*")
