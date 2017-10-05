3c8e5a196f29e30dd3442e45f0de683f9220dc5a
David Wendt <dcrkid@yahoo.com>
2017-10-04 23:15:31 -0600

    Add latin diacritic characters into the diacritic replacement list, removing the Japanese kana diacritics which were already present and buggy. I don't know if this is the ideal way to enter diacritics in other European languages but it's here regardless.

--- components/pausemenu/phoneime_diacritics.asm
+++ components/pausemenu/phoneime_diacritics.asm
@@ -27,29 +27,54 @@ PauseMenu_PhoneIMEApplyDiacritic::
 ;   index in the replacement table. That's it.
 ;   To build chains of characters, just make a replacement for each character in
 ;   the chain.
+
+;   LATIN-1 notes: We use the following diacritic order.
+;       a -> à -> á -> â -> ä -> ç -> ñ -> a
+;   Missing diacritic forms for specific letters will be skipped in the cycle.
+;   Other languages feel free to alter this order.
+;   English doesn't really use diacritics anyway, it's mostly for completeness
+;   sake.
 .searchTable
-    db $3D, $3E, $3F, $40, $41, $42, $43, $44
-    db $45, $46, $47, $48, $49, $4A, $4B, $51
-    db $52, $53, $1D, $54, $96, $97, $98, $7F
-    db $99, $06, $07, $08, $09, $0A, $0B, $0C
-    db $0D, $0E, $0F, $10, $11, $12, $13, $14
-    db $1A, $1B, $1C, $1D, $1E, $7C, $7D, $7E
-    db $7F, $80, $87, $88, $89, $8A, $8B, $8C
-    db $8D, $8E, $8F, $90, $91, $92, $93, $94
-    db $95, $9A, $9B, $9C, $84, $9D, $6D, $6E
-    db $6F, $70, $71, $72, $73, $74, $75, $76
-    db $77, $78, $79, $7A, $7B, $81, $82, $83
-    db $84, $85
+    db $41, $85, $80, $8F, $8A ; AÀÁÂÄA
+    db $45, $86, $81, $90, $8B ; EÈÉÊËE
+    db $49, $87, $82, $91, $8C ; IÌÍÎÏI
+    db $4F, $88, $83, $92, $8D ; OÒÓÔÖO
+    db $55, $89, $84, $93, $8E ; UÙÚÛÜU
+    db $61, $A5, $A0, $AF, $AA ; aàáâäa
+    db $65, $A6, $A1, $B0, $AB ; eèéêëe
+    db $69, $A7, $A2, $B1, $AC ; iìíîïi
+    db $6F, $A8, $A3, $B2, $AD ; oòóôöo
+    db $75, $A9, $A4, $B3, $AE ; uùúûüu
+    db $59, $94 ; YŸY
+    db $43, $95 ; CÇC
+    db $4E, $96 ; NÑN
+    db $79, $B4 ; yÿy
+    db $63, $B5 ; cçc
+    db $6E, $B6 ; nñn
+    db $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00
 .replacementTable
-    db $87, $88, $89, $8A, $8B, $8C, $8D, $8E
-    db $8F, $90, $91, $92, $93, $94, $95, $96
-    db $97, $98, $7F, $99, $9A, $9B, $9C, $84
-    db $9D, $6D, $6E, $6F, $70, $71, $72, $73
-    db $74, $75, $76, $77, $78, $79, $7A, $7B
-    db $7C, $7D, $7E, $7F, $80, $81, $82, $83
-    db $84, $85, $3D, $3E, $3F, $40, $41, $42
-    db $43, $44, $45, $46, $47, $48, $49, $4A
-    db $4B, $51, $52, $53, $1D, $54, $06, $07
-    db $08, $09, $0A, $0B, $0C, $0D, $0E, $0F
-    db $10, $11, $12, $13, $14, $1A, $1B, $1C
-    db $1D, $1E
+    db $85, $80, $8F, $8A, $41 ; AÀÁÂÄA
+    db $86, $81, $90, $8B, $45 ; EÈÉÊËE
+    db $87, $82, $91, $8C, $49 ; IÌÍÎÏI
+    db $88, $83, $92, $8D, $4F ; OÒÓÔÖO
+    db $89, $84, $93, $8E, $55 ; UÙÚÛÜU
+    db $A5, $A0, $AF, $AA, $61 ; aàáâäa
+    db $A6, $A1, $B0, $AB, $65 ; eèéêëe
+    db $A7, $A2, $B1, $AC, $69 ; iìíîïi
+    db $A8, $A3, $B2, $AD, $6F ; oòóôöo
+    db $A9, $A4, $B3, $AE, $75 ; uùúûüu
+    db $94, $59 ; YŸY
+    db $95, $43 ; CÇC
+    db $96, $4E ; NÑN
+    db $B4, $79 ; yÿy
+    db $B5, $63 ; cçc
+    db $B6, $6E ; nñn
+    db $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00
