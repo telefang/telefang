3b031413998ab97e411988b5b0ea74ff77418db2
andwhyisit <realmmaster26@hotmail.com>
2018-02-18 16:38:48 +1000

    This should in theory fix issue #14.

--- components/fusionlabevo/item_name_utils.asm
+++ components/fusionlabevo/item_name_utils.asm
@@ -3,32 +3,36 @@ INCLUDE "telefang.inc"
 SECTION "Fusion/Lab Evolution Name Copying", ROMX[$52A2], BANK[$2A]
 ;TODO: What do these entry points do differently?
 FusionLabEvo_LoadSpeciesNameEntryPointA::
-    ld de, W_MainScript_MessageArg3Old
+    ld de, W_SaveClock_NicknameStaging
     ld a, b
     jr FusionLabEvo_LoadSpeciesName
     
 FusionLabEvo_LoadSpeciesNameEntryPointB::
-    ld de, $CA00
+    ld de, W_MainScript_CenteredNameBuffer
     jr FusionLabEvo_LoadSpeciesName
     
 FusionLabEvo_LoadSpeciesNameEntryPointC::
-    ld de, W_MainScript_MessageArg3Old
+    ld de, W_SaveClock_NicknameStaging
     ld a, [$CAED]
     
 FusionLabEvo_LoadSpeciesName::
     ld hl, StringTable_denjuu_species
     ld c, a
     ld b, $0
-    sla c
-    rl b
-    sla c
-    rl b
-    sla c
-    rl b
+	call FusionLabEvo_Multiplier
+	call FusionLabEvo_Multiplier
+	call FusionLabEvo_Multiplier
+	call FusionLabEvo_Multiplier
     add hl, bc
     ld c, BANK(StringTable_denjuu_species)
-    ld b, $8
+    ld b, $10
     call Banked_Memcpy
     ld a, $E0
     ld [de], a
+    ret
+    
+SECTION "Fusion/Lab Evolution Name Index Multiplier", ROM0[$EC]
+FusionLabEvo_Multiplier::
+    sla c
+    rl b
     ret
