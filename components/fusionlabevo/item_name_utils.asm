3bdf536466785c1e308ef47e94d1106c59e63ba6
andwhyisit <realmmaster26@hotmail.com>
2018-02-21 21:01:10 +1000

    Change symbols for loading Denjuu names.

--- components/fusionlabevo/item_name_utils.asm
+++ components/fusionlabevo/item_name_utils.asm
@@ -3,16 +3,16 @@ INCLUDE "telefang.inc"
 SECTION "Fusion/Lab Evolution Name Copying", ROMX[$52A2], BANK[$2A]
 ;TODO: What do these entry points do differently?
 FusionLabEvo_LoadSpeciesNameEntryPointA::
-    ld de, W_SaveClock_NicknameStaging
+    ld de, W_MainScript_MessageArg3
     ld a, b
     jr FusionLabEvo_LoadSpeciesName
     
 FusionLabEvo_LoadSpeciesNameEntryPointB::
-    ld de, W_MainScript_CenteredNameBuffer
+    ld de, W_Map_LocationStaging
     jr FusionLabEvo_LoadSpeciesName
     
 FusionLabEvo_LoadSpeciesNameEntryPointC::
-    ld de, W_SaveClock_NicknameStaging
+    ld de, W_MainScript_MessageArg3
     ld a, [$CAED]
     
 FusionLabEvo_LoadSpeciesName::
