90e18e996d05421ce40e5fddcac5649b550263dd
obskyr <powpowd@gmail.com>
2018-02-25 12:54:58 +0100

    Merge remote-tracking branch 'upstream/patch' into field-guide-improvements

--- gfx/denjuu_stages.asm
+++ gfx/denjuu_stages.asm
@@ -5,3 +5,7 @@ DenjuuStageGfx::
 SECTION "gfx/denjuu_types.2bpp", ROMX[$5b40], BANK[$7d]
 DenjuuTypeGfx::
 	INCBIN "gfx/denjuu_types.2bpp"
+
+SECTION "gfx/denjuu_stages_zukan.2bpp", ROMX[$6000], BANK[$7d]
+DenjuuStageZukanGfx::
+	INCBIN "gfx/denjuu_stages_zukan.2bpp"
