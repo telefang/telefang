b733c3cf36b105c8d94a7ef6d9ca5549b685f4a8
andwhyisit <realmmaster26@hotmail.com>
2018-03-04 17:58:59 +1000

    Status gfx.

--- components/battle/status.asm
+++ components/battle/status.asm
@@ -3,3 +3,33 @@ INCLUDE "telefang.inc"
 SECTION "Status Effect Gfx Table", ROMX[$7E00], BANK[$1]
 DenjuuStatusEffectGfx::
 	INCBIN "components/battle/status.1bpp"
+	
+SECTION "Draw Status Effect Gfx", ROMX[$452D], BANK[$1]
+MainScript_ADVICE_DrawStatusEffectGfx::
+	push hl
+	ld de,DenjuuStatusEffectGfx
+	ld l,b
+	ld h,0
+	sla l
+	sla l
+	sla l
+	sla l
+	sla l
+	rl h
+	add hl,de
+	push hl
+	pop de
+	pop hl
+	ld b,$20
+
+.statusEffectWriteLoop
+	ld a,[de]
+	di
+	call YetAnotherWFB
+	ld [hli],a
+	ld [hli],a
+	ei
+	inc de
+	dec b
+	jr nz, .statusEffectWriteLoop
+	ret
