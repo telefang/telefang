83f22204b73375d151acf11bd49bf5a7ddae2fa9
andwhyisit <realmmaster26@hotmail.com>
2018-03-28 23:10:27 +1000

    The T-Fanger article code. It isn't hook up to anything yet.

--- components/encounter/articles.asm
+++ components/encounter/articles.asm
@@ -36,9 +36,80 @@ Encounter_ADVICE_OpponentNameAndArticleLoader::
 
 	ld a, $E0
 	ld [de], a
+	ld de, W_Map_LocationStaging + $10
+	ld [de], a
 	pop hl
 	pop de
 	pop bc
 	pop af
 	ret
 	
+SECTION "Encounter Screen TFanger Article Table Advice", ROMX[$5E40], BANK[$1C]
+Encounter_ADVICE_TFangerNameAndArticleLoader::
+	call StringTable_LoadName75
+	push bc
+	push de
+	push hl
+	push af
+	ld a, [W_StringTable_ROMTblIndex]
+	ld e, a
+	ld d, 0
+	sla e
+	rl d
+	push de
+	ld hl, Encounter_ADVICE_TFangerArticleTable
+	add hl, de
+	
+	ld a, [hli]
+	ld c, a
+	ld a, [hli]
+	ld b, a
+	push bc
+	pop hl
+	ld de, W_Map_LocationStaging
+	ld b, $F
+	
+.copyLoopA
+	ld a, [hli]
+	cp $E0
+	jr z, .abandonLoopA
+	ld [de], a
+	inc de
+	dec b
+	jr nz, .copyLoopA
+	
+.abandonLoopA
+	
+	ld a, $E0
+	ld [de], a
+	pop de
+	ld hl, Encounter_ADVICE_TFangerArticleTableB
+	add hl, de
+	ld a, [hli]
+	ld c, a
+	ld a, [hli]
+	ld b, a
+	push bc
+	pop hl
+	ld de, W_Map_LocationStaging + $10
+	ld b, $F
+	
+.copyLoopB
+	ld a, [hli]
+	cp $E0
+	jr z, .abandonLoopB
+	ld [de], a
+	inc de
+	dec b
+	jr nz, .copyLoopB
+	
+.abandonLoopB
+	
+	ld a, $E0
+	ld [de], a
+	
+	pop af
+	pop hl
+	pop de
+	pop bc
+	ret
