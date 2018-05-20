1c79399961418ea36b89acf4f43a6a3d073c6e4e
David Wendt <dcrkid@yahoo.com>
2018-05-20 17:52:29 -0400

    Add code to commit staged CGB palettes into SGB colors

--- components/sgb/convert_colours.asm
+++ components/sgb/convert_colours.asm
@@ -1,6 +1,121 @@
 INCLUDE "telefang.inc"
 
-SECTION "GBC Colour To SGB Colour Converter", ROMX[$7430], BANK[$1]
+SECTION "GBC Colour To SGB Colour Converter", ROMX[$7300], BANK[$1]
+;Takes two given staged CGB palettes, converts them to SGB colorspace, and commits
+;them in a given set of SGB palette slots.
+; 
+; A=SGB command to use (one of M_SGB_Pal01, Pal23, Pal03, Pal12)
+; B=First CGB palette (in staging area)
+; C=Second CGB palette (in staging area)
+; 
+; Returns nothing
+; 
+PatchUtils_CommitStagedCGBToSGB::
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Command], a
+    
+    ld a, W_LCDC_CGBStagingBGPaletteArea & $FF
+    swap b
+    add a, b
+    ld l, a
+    ld a, W_LCDC_CGBStagingBGPaletteArea >> 8
+    adc a, 0
+    ld h, a
+    
+    ld a, [hli]
+    ld e, a
+    ld a, [hli]
+    ld d, a
+    
+    call PatchUtils_ColourToSGB
+    
+    ld a, d
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal0Color0 + 0], a
+    ld a, e
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal0Color0 + 1], a
+    
+    ld a, [hli]
+    ld e, a
+    ld a, [hli]
+    ld d, a
+    
+    call PatchUtils_ColourToSGB
+    
+    ld a, d
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal0Color1 + 0], a
+    ld a, e
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal0Color1 + 1], a
+    
+    ld a, [hli]
+    ld e, a
+    ld a, [hli]
+    ld d, a
+    
+    call PatchUtils_ColourToSGB
+    
+    ld a, d
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal0Color2 + 0], a
+    ld a, e
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal0Color2 + 1], a
+    
+    ld a, [hli]
+    ld d, [hl]
+    ld e, a
+    
+    call PatchUtils_ColourToSGB
+    
+    ld a, d
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal0Color3 + 0], a
+    ld a, e
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal0Color3 + 1], a
+    
+    ld a, c
+    swap a
+    add a, 2
+    add a, W_LCDC_CGBStagingBGPaletteArea & $FF
+    ld l, a
+    ld a, W_LCDC_CGBStagingBGPaletteArea >> 8
+    adc a, 0
+    ld h, a
+    
+    ld a, [hli]
+    ld e, a
+    ld a, [hli]
+    ld d, a
+    
+    call PatchUtils_ColourToSGB
+    
+    ld a, d
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal1Color1 + 0], a
+    ld a, e
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal1Color1 + 1], a
+    
+    ld a, [hli]
+    ld e, a
+    ld a, [hli]
+    ld d, a
+    
+    call PatchUtils_ColourToSGB
+    
+    ld a, d
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal1Color2 + 0], a
+    ld a, e
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal1Color2 + 1], a
+    
+    ld a, [hli]
+    ld d, [hl]
+    ld e, a
+    
+    call PatchUtils_ColourToSGB
+    
+    ld a, d
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal1Color3 + 0], a
+    ld a, e
+    ld [W_SGB_SpotPalette + M_SGB_Pal01Pal1Color3 + 1], a
+    
+    xor a
+    ld [W_SGB_SpotPalette + $F], a
+    ret
+
 PatchUtils_ColourToSGB::
 
 	push af
