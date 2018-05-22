93e2d6d4ff3fa03a62b62458824a9a714e65082c
andwhyisit <realmmaster26@hotmail.com>
2018-05-22 15:14:25 +1000

    Relocate the font data to prevent overlap.

--- components/mainscript/font.asm
+++ components/mainscript/font.asm
@@ -11,7 +11,7 @@ SECTION "Main Script Narrow Font", ROMX[$7600], BANK[$1]
 MainScript_NarrowFont::
     INCBIN "build/gfx/narrow_font.1bpp", 0, NUM_CHARACTERS * 8
 	
-SECTION "Main Script Bold Font", ROMX[$6E00], BANK[$1]
+SECTION "Main Script Bold Font", ROMX[$6D00], BANK[$1]
 MainScript_BoldFont::
 ; The bold font excludes system characters (characters B8 to D2) to save space.
     INCBIN "build/gfx/bold_font.1bpp", 0, NUM_NON_SYSTEM_CHARACTERS * 8
