d28ac0be4d8398a7fd3885f460c37ebb324a4c6d
andwhyisit <realmmaster26@hotmail.com>
2018-06-14 23:07:12 +1000

    Proper text rendering for the credits plus a bold text control code because... why not?

--- components/credit/credits.asm
+++ components/credit/credits.asm
@@ -8,7 +8,7 @@ W_Credits_TextHorizontalPosition:: ds 1
 W_Credits_TextVerticalPosition:: ds 1
 W_Credits_TextCharacterNumber:: ds 1
 
-SECTION "Credits Message Table", ROMX[$62DD], BANK[$E]
+SECTION "Credits Message Table", ROMX[$6D7A], BANK[$E]
 Credits_MessageTable::
 ; Included for reference.
 
@@ -16,12 +16,7 @@ SECTION "Credits - Initiate First Screen", ROMX[$60FD], BANK[$E]
 Credits_InitiateFirstScreen::
 	ld a, $E
 	ld [W_PreviousBank], a
-	ld a, 1
-	ld [W_OAM_SpritesReady], a
-	ld a, 0
-	ld [$C386], a
-	ld a, 0
-	ld [$C38B], a
+	call Credits_ADVICE_Prepare
 	ld a, 3
 	ld [$C381], a
 	ld a, 5
@@ -30,26 +25,36 @@ Credits_InitiateFirstScreen::
 	ld [W_Credits_TextAddressBuffer], a
 	ld a, [Credits_MessageTable + 1]
 	ld [W_Credits_TextAddressBuffer + 1], a
-	ld a, 0
-	ld [W_MainScript_TextStyle], a
 	ld bc, 1
 	ld a, $33
 	ld hl, $6601
 	call CallBankedFunction_int
-	ld a, 0
-	ld [$C38A], a
 	call Credits_ParseAndRenderText
 	call $6145
 	jp System_ScheduleNextSubState
+	nop
+	nop
+	nop
+	nop
+	
+Credits_ADVICE_VWFNextTile:
+	ld a, [W_MainScript_VWFOldTileMode]
+	cp 1
+	ld a, [W_MainScript_TilesDrawn]
+	jr c, .noIncrement
+	inc a
+	ld [W_MainScript_TilesDrawn], a
+	
+.noIncrement
+	ret
+	
+	nop
+	nop
+	nop
 	
 SECTION "Credits - Parse and Render Text", ROMX[$6286], BANK[$E]
 Credits_ParseAndRenderText::
-	ld a, 0
-	ld [W_Credits_TextHorizontalPosition], a
-	ld a, 0
-	ld [W_Credits_TextVerticalPosition], a
-	ld a, 1
-	ld [W_Credits_TextCharacterNumber], a
+	call Credits_ADVICE_PrepareForDrawing
 
 .nextCharacter
 	ld a, [W_Credits_TextAddressBuffer + 1]
@@ -60,33 +65,30 @@ Credits_ParseAndRenderText::
 .postLinebreak
 	ld a, [hli]
 	ld c, a
-	cp a, $E1
+	cp $E1
 	jr z, .endCodeFound
-	cp a, $E2
+	cp $E2
 	jr z, .lineBreakFound
+	cp $F2
+	jr z, .boldFontRequested
 	ld a, h
 	ld [W_Credits_TextAddressBuffer + 1], a
 	ld a, l
 	ld [W_Credits_TextAddressBuffer], a
-	call Credits_DrawCharacter
-	ld a, [W_Credits_TextHorizontalPosition]
-	inc a
-	ld [W_Credits_TextHorizontalPosition], a
-	ld a, [W_Credits_TextCharacterNumber]
-	inc a
-	ld [W_Credits_TextCharacterNumber], a
+	call Credits_ADVICE_DrawCharacter
 	jr .nextCharacter
 
+.boldFontRequested
+	ld a, 2
+	ld [W_MainScript_ADVICE_FontToggle], a
+	jr .postLinebreak
+
 .lineBreakFound
-	ld a, [W_Credits_TextVerticalPosition]
-	inc a
-	ld [W_Credits_TextVerticalPosition], a
-	ld a, 0
-	ld [W_Credits_TextHorizontalPosition], a
-	inc hl
+	call Credits_ADVICE_Newline
 	jr .postLinebreak
 
 .endCodeFound
+	call Credits_ADVICE_Newline
 	ld b, [hl]
 	inc hl
 	ld a, h
@@ -95,9 +97,32 @@ Credits_ParseAndRenderText::
 	ld [W_Credits_TextAddressBuffer], a
 	xor a
 	ret
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
 
 SECTION "Credits - Draw Character", ROMX[$622D], BANK[$E]
 Credits_DrawCharacter::
+; Legacy code. Most likely safe to remove, but we have room to burn, so it is easier to keep it.
 	ld hl, $9000
 	ld a, [W_Credits_TextCharacterNumber]
 	ld e, a
@@ -119,7 +144,7 @@ Credits_DrawCharacter::
 	ld a, [W_Credits_TextHorizontalPosition]
 	add e
 	ld e, a
-	ld d, $00
+	ld d, 0
 	add hl, de
 	ld a, [$C382]
 	ld e, a
@@ -147,3 +172,860 @@ Credits_DrawCharacter::
 	jr nz, .jpA
 	ld [hl], b
 	ret
+
+SECTION "Credits - Patch Utils", ROMX[$62DD], BANK[$E]
+Credits_ADVICE_Prepare::
+	xor a
+	ld [W_MainScript_TilesDrawn], a
+	ld [W_Credits_TextHorizontalPosition], a
+	ld [W_Credits_TextVerticalPosition], a
+	ld [W_MainScript_ADVICE_FontToggle], a
+	ld [W_MainScript_TextStyle], a
+	ld [$C386], a
+	ld [$C38B], a
+	ld [$C38A], a
+	inc a
+	ld [W_Credits_TextCharacterNumber], a
+	ld [W_MainScript_TileBaseIdx], a
+	ld [W_OAM_SpritesReady], a
+	ret
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+
+Credits_ADVICE_DrawCharacter::
+	ld a, [W_MainScript_TilesDrawn]
+	ld b, a
+	ld a, [W_MainScript_TileBaseIdx]
+	add b
+	call LCDC_TileIdx2Ptr
+	ld a, c
+	call Banked_MainScript_DrawLetter
+	call Credits_ADVICE_VWFNextTile
+	ret
+	nop
+	nop
+	nop
+	nop
+
+Credits_ADVICE_Newline::
+	xor a
+	ld [W_MainScript_VWFLetterShift], a
+	ld [W_MainScript_VWFCurrentLetter], a
+	ld [W_MainScript_ADVICE_FontToggle], a
+	ld [W_Credits_TextHorizontalPosition], a
+	push hl
+	ld hl, $CFD0
+	ld b, $10
+
+.clearCompositeAreaLoop
+	ld [hli], a
+	dec b
+	jr nz, .clearCompositeAreaLoop
+	ld a, [W_MainScript_TilesDrawn]
+	inc a
+	ld b, a
+	ld a, [W_MainScript_TileBaseIdx]
+	add b
+	ld [W_MainScript_TileBaseIdx], a
+
+.tileMapLoop
+	ld hl, $9800
+	ld a, [$C381]
+	ld e, a
+	ld a, [W_Credits_TextHorizontalPosition]
+	inc a
+	ld [W_Credits_TextHorizontalPosition], a
+	dec a
+	add e
+	ld e, a
+	ld d, 0
+	add hl, de
+	ld a, [$C382]
+	ld e, a
+	ld a, [W_Credits_TextVerticalPosition]
+	add e
+	ld e, a
+	sla e
+	rl d
+	sla e
+	rl d
+	sla e
+	rl d
+	sla e
+	rl d
+	sla e
+	rl d
+	add hl, de
+	ld a, [W_Credits_TextCharacterNumber]
+	ld c, a
+
+.jpA
+	ldh a, [REG_STAT]
+	and a, 2
+	jr nz, .jpA
+	ld [hl], c
+	ld a, c
+	inc a
+	ld [W_Credits_TextCharacterNumber], a
+	dec b
+	jp nz, .tileMapLoop
+	pop hl
+	xor a
+	ld [W_Credits_TextHorizontalPosition], a
+	ld [W_MainScript_TilesDrawn], a
+
+; Handle successive linebreaks without repeating the logic above.
+
+.linebreakLoop
+	ld a, [W_Credits_TextVerticalPosition]
+	inc a
+	ld [W_Credits_TextVerticalPosition], a
+	ld a, [hli]
+	cp $E2
+	jr z, .endCodeFound
+	dec hl
+	call Credits_ADVICE_ClearFirstTile
+	ret
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+
+Credits_ADVICE_ClearFirstTile::
+; Clear the first tile on a line in case that line wasn't written to prior to a linebreak.
+	push hl
+	ld a, [W_MainScript_TileBaseIdx]
+	call LCDC_TileIdx2Ptr
+	ld b, $10
+
+.jpA
+	ldh a, [REG_STAT]
+	and a, 2
+	jr nz, .jpA
+	xor a
+	ld [hl], a
+	inc hl
+	dec b
+	jr nz, .jpA
+	pop hl
+	ret
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	
+Credits_ADVICE_PrepareForDrawing::
+	xor a
+	ld [W_MainScript_TilesDrawn], a
+	ld [W_Credits_TextHorizontalPosition], a
+	ld [W_Credits_TextVerticalPosition], a
+	ld [W_MainScript_VWFOldTileMode], a
+	inc a
+	ld [W_Credits_TextCharacterNumber], a
+	ld [W_MainScript_TileBaseIdx], a
+	call Credits_ADVICE_ClearFirstTile
+	ret
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
