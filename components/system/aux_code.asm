ecad04d9c7a4d73ad7788d9d0de3a54f9683a5a1
andwhyisit <realmmaster26@hotmail.com>
2018-06-20 22:10:24 +1000

    Here's that mid-battle article loader I've been putting off.

--- components/system/aux_code.asm
+++ components/system/aux_code.asm
@@ -123,13 +123,23 @@ Banked_Status_ADVICE_StateExit::
 	jp Status_ADVICE_StateExit
 	nop
 
-;Sections beyond this point are at the END of Bank 1 and likely not conflicting
+;The next two are at the END of Bank 1 and likely not conflicting
 ;with your section.
 Banked_MainScript_ADVICE_DrawNarrowLetter::
 	jp MainScript_ADVICE_DrawNarrowLetter
 	nop
+
 Banked_Battle_ADVICE_ParsePluralState::
 	jp Battle_ADVICE_ParsePluralState
+	nop
+
+;components/battle/articles.asm
+Banked_Battle_ADVICE_BattleNoArticle::
+	jp Battle_ADVICE_BattleNoArticle
+	nop
+
+Banked_Battle_ADVICE_BattleArticle::
+	jp Battle_ADVICE_BattleArticle
 	nop
 
 SECTION "Patch Utilities - Auxiliary Code", ROMX[$4100], BANK[$1]
