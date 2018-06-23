66149699fade8ef3fc549addfc20a0d2a2205306
David Wendt <dcrkid@yahoo.com>
2018-06-23 18:39:54 -0400

    Fix bank of phone frame graphics.

--- gfx/phones/phone_gfx.asm
+++ gfx/phones/phone_gfx.asm
@@ -1,10 +1,10 @@
-SECTION "Phone border graphics", ROMX[$5D5F], BANK[$7a]
+SECTION "Phone border graphics", ROMX[$5D5F], BANK[$37]
 PhoneGfx::
 	INCBIN "build/gfx/phones/0/phone.2bpp"
 	INCBIN "build/gfx/phones/1/phone.2bpp"
 	INCBIN "build/gfx/phones/2/phone.2bpp"
 
-SECTION "Phone model number graphics", ROMX[$6E5F], BANK[$7a]
+SECTION "Phone model number graphics", ROMX[$6E5F], BANK[$37]
 PhoneModelNumbersGfx::
 	INCBIN "build/gfx/phones/0/model_numbers.2bpp"
 	INCBIN "build/gfx/phones/1/model_numbers.2bpp"
