347db09ff29b03efe8e03f46cb2d40a27fa76ffa
obskyr <powpowd@gmail.com>
2018-10-17 19:05:17 +0200

    Auto-formatting tools for the live translation sheet
    These'll make translating some things a whole lot smoother! It'll also be easier to be detail-oriented, which you know that we're all about.

---
+++ rip_scripts/sheets/font.js
@@ -0,0 +1,155 @@
+"use strict";
+
+var METRIC_SHEET_NAME = "Font metrics";
+
+var CHARMAP_URL = "https://raw.githubusercontent.com/telefang/telefang/patch/charmap.asm";
+var FONT_NAMES = ["normal", "bold", "narrow"];
+var METRIC_CSV_URLS = {
+  normal: "https://raw.githubusercontent.com/telefang/telefang/patch/components/mainscript/font.tffont.csv",
+  bold: "https://raw.githubusercontent.com/telefang/telefang/patch/components/mainscript/font_bold.tffont.csv",
+  narrow: "https://raw.githubusercontent.com/telefang/telefang/patch/components/mainscript/font_narrow.tffont.csv"
+};
+
+function updateMetricSheet() {
+  var sheet = getMetricSheet();
+  
+  var r = UrlFetchApp.fetch(CHARMAP_URL);
+  var charmap = r.getContentText('utf-8');
+  charmap = parseCharmap(charmap);
+  
+  var width_lists = [];
+  for (var i = 0; i < FONT_NAMES.length; i++) {
+    r = UrlFetchApp.fetch(METRIC_CSV_URLS[FONT_NAMES[i]]);
+    var widths = parseMetricCsv(r.getContentText('utf-8'));
+    width_lists.push(widths);
+  }
+  
+  var values = []
+  for (var i = 0; i < charmap.length; i++) {
+    var char = charmap[i][0];
+    // Escape Sheets's special characters.
+    if (char[0] === '=' || char[0] === '\'') {char = '\'' + char;}
+    var code = charmap[i][1];
+    var row = [char];
+    for (var j = 0; j < width_lists.length; j++) {
+      row.push(width_lists[j][code]);
+    }
+    values.push(row);
+  }
+  
+  var numRows = sheet.getFrozenRows() + values.length;
+  var numCols = sheet.getFrozenColumns() + values[0].length;
+  setSheetDimensions(sheet, numRows, numCols);
+  
+  var range = sheet.getRange(sheet.getFrozenRows() + 1, sheet.getFrozenColumns() + 1, values.length, values[0].length);
+  range.setValues(values);
+  
+  // And some formatting as the cherry on top.
+  var textRange = sheet.getRange(range.getRow(), range.getColumn(), range.getNumRows(), 1);
+  var numRange = sheet.getRange(range.getRow(), range.getColumn() + 1, range.getNumRows(), range.getNumColumns() - 1);
+  textRange.setNumberFormat('@');
+  numRange.setNumberFormat('#');
+}
+
+function getFontMetrics() {
+  var sheet = getMetricSheet();
+  var dataRange = sheet.getDataRange();
+  var totalRows = dataRange.getLastRow();
+  var frozenRows = sheet.getFrozenRows();
+  var frozenCols = sheet.getFrozenColumns();
+  
+  var chars = getChars(sheet, totalRows, frozenRows, frozenCols);
+  
+  var fonts = {};
+  for (var i = 0; i < FONT_NAMES.length; i++) {
+    var width_map = fonts[FONT_NAMES[i]] = {};
+    var widths = sheet.getRange(frozenRows + 1, frozenCols + 1 + 1 + i, totalRows - frozenRows).getValues();
+    for (var j = 0; j < chars.length; j++) {
+      width_map[chars[j]] = widths[j][0];
+    }
+  }
+  
+  return fonts;
+}
+
+function getChars(sheet, totalRows, frozenRows, frozenCols) {
+  if (typeof sheet === 'undefined' ||
+      typeof totalRows === 'undefined' ||
+      typeof frozenRows === 'undefined' ||
+      typeof frozenCols === 'undefined') {
+    sheet = getMetricSheet();
+    var dataRange = sheet.getDataRange();
+    totalRows = dataRange.getLastRow();
+    frozenRows = sheet.getFrozenRows();
+    frozenCols = sheet.getFrozenColumns();
+  }
+  
+  var chars = sheet.getRange(frozenRows + 1, frozenCols + 1, totalRows - frozenRows).getValues();
+  chars = chars.map(function(x) {return x[0];});
+  return chars;
+}
+
+function getMetricSheet() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(METRIC_SHEET_NAME);
+  if (!sheet) {throw "Sheet \"" + METRIC_SHEET_NAME + "\" not found.";}
+  return sheet;
+}
+
+function setSheetDimensions(sheet, numRows, numCols) {
+  var currentRows = sheet.getMaxRows();
+  var currentCols = sheet.getMaxColumns();
+
+  if (currentRows > numRows) {
+    sheet.deleteRows(numRows + 1, currentRows - numRows);
+  } else if (currentRows < numRows) {
+    sheet.insertRowsAfter(currentRows, numRows - currentRows);
+  }
+  
+  if (currentCols > numCols) {
+    sheet.deleteColumns(numCols + 1, currentCols - numCols);
+  } else if (currentCols < numCols) {
+    sheet.insertColumnsAfter(currentCols, numCols - currentCols);
+  }
+}
+
+// The """ charmap is actually incorrect RGBDS code, but exists
+// in Telefang's charmap file since it goes through a Python
+// script and never actually through RGBDS.
+var CHARMAP_LINE_RE = /^(?:charmap "("|[^"]+)",\s*(|\$|%|&)([0-9A-Fa-f]+))?\s*(?:;.*)?$/;
+function parseCharmap(s) {
+  var charmap = [];
+  var lines = s.split('\n');
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    var m = line.match(CHARMAP_LINE_RE);
+    if (!m[1]) {continue;}
+    
+    var char = m[1];
+    char = {'\\n': '\n'}[char] || char;
+    var base = {'': 10, '$': 16, '%': 2, '&': 8}[m[2]];
+    var number = parseInt(m[3], base);
+    charmap.push([char, number]);
+  }
+  return charmap;
+}
+
+function parseMetricCsv(s) {
+  var table = parseCsv(s);
+  table = table.slice(1); // Remove header row.
+  table = table.map(function(row) {return row.slice(1);}); // Remove header column.
+  
+  // Only handles hexadecimal numbers prefixed by a $.
+  table = table.map(function(row) {
+    return row.map(function(num) {return parseInt(num.slice(1), 16);});
+  });
+  
+  return table.reduce(function(x, y) {return x.concat(y);});
+}
+
+function parseCsv(s) {
+  // Only handles super simple CSVs without quoted fields and all that.
+  var parsed = s.split('\n');
+  if (!parsed[parsed.length - 1]) {parsed = parsed.slice(0, parsed.length - 1);}
+  parsed = parsed.map(function(line) {return line.split(',');})
+  return parsed;
+}
---
+++ rip_scripts/sheets/readme.md
@@ -0,0 +1,3 @@
+# Tools for Google Sheets
+
+Google Apps Script tools used to make the translation sheet more pleasant and fun. Use [clasp](https://github.com/google/clasp) to push these to the sheet – anytime after having logged in using `clasp login`, run `clasp push` to put them into use. Provided you have edit access to the sheet, that is, of course!
