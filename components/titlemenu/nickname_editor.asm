6a59b0cfcf9003434870561b94f3b5894fac2573
andwhyisit <realmmaster26@hotmail.com>
2019-03-24 10:34:08 +1000

    Time to kill this orphaned code and reclaim some space in bank 4.

--- components/titlemenu/nickname_editor.asm
+++ components/titlemenu/nickname_editor.asm
@@ -1,58 +1,70 @@
 INCLUDE "telefang.inc"
 
-SECTION "Title Menu Nickname Loader", ROMX[$7D8C], BANK[$4]
-TitleMenu_LoadDenjuuNicknameIntoBuffer::
-    ld c, a
-    ld b, 0
-    ld de, M_SaveClock_DenjuuNicknameSize
-    
-    ld a, BANK(TitleMenu_ADVICE_LoadDenjuuNicknameIntoBuffer)
-    call Banked_TitleMenu_ADVICE_LoadDenjuuNicknameIntoBuffer
+SECTION "Title Menu Nickname Ex-Loader Nops", ROMX[$7D8C], BANK[$4]
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
     nop
     nop
     
-    ld b, BANK(S_SaveClock_NicknameArray)
-    call TitleMenu_EnterSRAM
-    
-    ld de, W_TitleMenu_NameBuffer
-    ld bc, M_SaveClock_DenjuuNicknameSize
-    call memcpy
-    
-    call TitleMenu_ExitSRAM
-    
-    ld hl, W_TitleMenu_NameBuffer
-    ld b, 0
-    ld c, M_SaveClock_DenjuuNicknameSize
-    
-.reterminateLoop
-    ld a, [hl]
-    
-    cp $E0
-    jr z, .changeTerminator
-    inc b
-    jr .noChangeTerminator
-    
-.changeTerminator
-    xor a
-    ld [hl], a
-    
-.noChangeTerminator
-    inc hl
-    dec c
-    
-    jr nz, .reterminateLoop
-    
-    ;Position the cursor at the end of the string.
-    ld a, b
-    cp (M_SaveClock_DenjuuNicknameSize - 1)
-    jr c, .setCursorPos
-    
-    ld a, (M_SaveClock_DenjuuNicknameSize - 1)
-.setCursorPos
-    ld [W_PauseMenu_SelectedMenuItem], a
-    
-    ret
-    
+SECTION "Title Menu Nickname Saver", ROMX[$7DCA], BANK[$4]
 TitleMenu_SaveDenjuuNicknameFromBuffer::
     ld c, a
     ld b, 0
