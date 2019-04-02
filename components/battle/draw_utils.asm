6863a72882c9d6f215bfb375f532a1fdfc2cf3d0
andwhyisit <realmmaster26@hotmail.com>
2019-04-03 01:18:45 +1000

    Fix for wrong address.

--- components/battle/draw_utils.asm
+++ components/battle/draw_utils.asm
@@ -1,6 +1,6 @@
 INCLUDE "telefang.inc"
 
-SECTION "Battle Misc Draw Utils 1", ROMX[$43F9], BANK[$5]
+SECTION "Battle Misc Draw Utils 1", ROMX[$400A], BANK[$5]
 Battle_ExpandNumericalTiles::
     ld a, $F0
     ld [W_Status_NumericalTileIndex], a
