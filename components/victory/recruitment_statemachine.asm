5807ea76b8dee48d99063b760f0d3391e15bc1be
andwhyisit <realmmaster26@hotmail.com>
2019-04-04 22:43:27 +1000

    The other half of the recruitment screen disassembly.

--- components/victory/recruitment_statemachine.asm
+++ components/victory/recruitment_statemachine.asm
@@ -12,15 +12,15 @@ Victory_RecruitmentStateMachine::
     dw Victory_SubStateRecruitmentFadeInAndMemoryMarker
     dw Victory_SubStateRecruitmentGotNumberMessage
     dw Victory_SubStateRecruitmentCheckCapacityAndDrawNumber
-    dw $51DC
-    dw $5202
-    dw $5297
-    dw $52D3
-    dw $52E7
+    dw Victory_SubStateRecruitmentHideNumberOnInput
+    dw Victory_SubStateRecruitmentSaveNumberMessageAndInputHandler
+    dw Victory_SubStateRecruitmentSaveNumberParseResponse
+    dw Victory_SubStateRecruitmentFadeOutForExit
+    dw Victory_SubStateRecruitmentExit
     dw Victory_SubStateRecruitmentWowYoureStrongMessage
     dw Victory_SubStateRecruitmentPhoneMemoryFullMessage
-    dw $52F1
-    dw $538E
+    dw Victory_SubStateRecruitmentGiveNicknameMessageAndInputHandler
+    dw Victory_SubStateRecruitmentOpenNicknameScreen
 
 Victory_RecruitmentPhoneMemoryTable::
     db $46, $8C, $F0
@@ -205,4 +205,251 @@ Victory_SubStateRecruitmentPhoneMemoryFullMessage::
     ld [W_Victory_UserSelection], a
     ld a, 6
     ld [W_LateDenjuu_SubSubState], a
+    ret
+
+Victory_SubStateRecruitmentHideNumberOnInput::
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_A + M_JPInput_B
+    ret z
+    ld a, 3
+    ld [W_Sound_NextSFXSelect], a
+    ld c, $63
+    call Battle_QueueMessage
+    xor a
+    ld [W_Victory_UserSelection], a
+    call Victory_PlaceChoiceCursor
+    ld a, 0
+    ld [W_PauseMenu_SelectedCursorType], a
+    call LCDC_BeginAnimationComplex
+    ld a, [W_LateDenjuu_SubSubState]
+    inc a
+    ld [W_LateDenjuu_SubSubState], a
+    ret
+
+Victory_SubStateRecruitmentSaveNumberMessageAndInputHandler::
+    call LCDC_IterateAnimationComplex
+    call Banked_MainScriptMachine
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_Left
+    jr z, .leftNotPressed
+    ld a, 2
+    ld [W_Sound_NextSFXSelect], a
+    ld a, [W_Victory_UserSelection]
+    cp 0
+    jr z, .moveCursorRightOnLeft
+    xor a
+    ld [W_Victory_UserSelection], a
+    jp Victory_PlaceChoiceCursor
+
+.moveCursorRightOnLeft
+    ld a, 1
+    ld [W_Victory_UserSelection], a
+    jp Victory_PlaceChoiceCursor
+
+.leftNotPressed
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_Right
+    jr z, .rightNotPressed
+    ld a, 2
+    ld [W_Sound_NextSFXSelect], a
+    ld a, [W_Victory_UserSelection]
+    cp 1
+    jr z, .moveCursorLeftOnRight
+    ld a, 1
+    ld [W_Victory_UserSelection], a
+    jp Victory_PlaceChoiceCursor
+
+.moveCursorLeftOnRight
+    xor a
+    ld [W_Victory_UserSelection], a
+    jp Victory_PlaceChoiceCursor
+
+.rightNotPressed
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_B
+    jr z, .bNotPressed
+    ld a, 3
+    ld [W_Sound_NextSFXSelect], a
+    jr .noSelected
+
+.bNotPressed
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_A
+    ret z
+    ld a, 3
+    ld [W_Sound_NextSFXSelect], a
+    ld a, [W_Victory_UserSelection]
+    cp 1
+    jr z, .noSelected
+    ld c, $64
+    jr .yesSelected
+
+.noSelected
+    ld a, 1
+    ld [W_Victory_UserSelection], a
+    call SaveClock_EnterSRAM2
+    ld hl, $A001
+    ld a, [$D4A7]
+    call Battle_IndexStatisticsArray
+    ld a, 0
+    ld [hl], a
+    ld c, $65
+
+.yesSelected
+    call Battle_QueueMessage
+    ld a, 0
+    ld [W_MetaSpriteConfig1], a
+    ld a, 1
+    ld [W_OAM_SpritesReady], a
+    ld a, [W_LateDenjuu_SubSubState]
+    inc a
+    ld [W_LateDenjuu_SubSubState], a
+    ret
+
+Victory_SubStateRecruitmentSaveNumberParseResponse::
+    call Banked_MainScriptMachine
+    ld a, [W_MainScript_State]
+    cp 9
+    ret nz
+    ld a, [W_Victory_UserSelection]
+    cp 0
+    jr nz, .noSelected
+    ld c, $8F
+    call Battle_QueueMessage
+    xor a
+    ld [W_Victory_UserSelection], a
+    call Victory_PlaceChoiceCursor
+    ld a, 0
+    ld [W_PauseMenu_SelectedCursorType], a
+    call LCDC_BeginAnimationComplex
+    ld a, $B
+    ld [W_LateDenjuu_SubSubState], a
+    ret
+
+.noSelected
+    ld a, 4
+    call Banked_LCDC_SetupPalswapAnimation
+    ld a, $10
+    ld [$CF96], a
+    ld a, [W_LateDenjuu_SubSubState]
+    inc a
+    ld [W_LateDenjuu_SubSubState], a
+    ret
+
+Victory_SubStateRecruitmentFadeOutForExit::
+    ld a, 1
+    call Banked_LCDC_PaletteFade
+    or a
+    ret z
+    ld a, 0
+    ld [W_ShadowREG_HBlankSecondMode], a
+    ld a, [W_LateDenjuu_SubSubState]
+    inc a
+    ld [W_LateDenjuu_SubSubState], a
+    ret
+
+Victory_SubStateRecruitmentExit::
+    xor a
+    ld [W_LateDenjuu_SubSubState], a
+    ld a, 8
+    ld [W_Battle_SubSubState], a
+    ret
+
+; 1D:52F1
+
+Victory_SubStateRecruitmentGiveNicknameMessageAndInputHandler::
+    call LCDC_IterateAnimationComplex
+    call Banked_MainScriptMachine
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_Left
+    jr z, .leftNotPressed
+    ld a, 2
+    ld [W_Sound_NextSFXSelect], a
+    ld a, [W_Victory_UserSelection]
+    cp 0
+    jr z, .moveCursorRightOnLeft
+    xor a
+    ld [W_Victory_UserSelection], a
+    jp Victory_PlaceChoiceCursor
+
+.moveCursorRightOnLeft
+    ld a, 1
+    ld [W_Victory_UserSelection], a
+    jp Victory_PlaceChoiceCursor
+
+.leftNotPressed
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_Right
+    jr z, .rightNotPressed
+    ld a, 2
+    ld [W_Sound_NextSFXSelect], a
+    ld a, [W_Victory_UserSelection]
+    cp 1
+    jr z, .moveCursorLeftOnRight
+    ld a, 1
+    ld [W_Victory_UserSelection], a
+    jp Victory_PlaceChoiceCursor
+
+.moveCursorLeftOnRight
+    xor a
+    ld [W_Victory_UserSelection], a
+    jp Victory_PlaceChoiceCursor
+
+.rightNotPressed
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_B
+    jr z, .bNotPressed
+    ld a, 0
+    ld [W_MetaSpriteConfig1], a
+    ld [$C0C0], a
+    ld a, 1
+    ld [W_OAM_SpritesReady], a
+    ld a, 3
+    ld [W_Sound_NextSFXSelect], a
+    jr .noSelected
+
+.bNotPressed
+    ldh a, [H_JPInput_Changed]
+    and M_JPInput_A
+    ret z
+    ld a, 3
+    ld [W_Sound_NextSFXSelect], a
+    ld a, 0
+    ld [W_MetaSpriteConfig1], a
+    ld [$C0C0], a
+    ld a, 1
+    ld [W_OAM_SpritesReady], a
+    ld a, [W_Victory_UserSelection]
+    cp 1
+    jr z, .noSelected
+    ld a, 4
+    call Banked_LCDC_SetupPalswapAnimation
+    ld a, [W_LateDenjuu_SubSubState]
+    inc a
+    ld [W_LateDenjuu_SubSubState], a
+    ret
+
+.noSelected
+    ld a, 4
+    call Banked_LCDC_SetupPalswapAnimation
+    ld a, $10
+    ld [$CF96], a
+    ld a, 7
+    ld [W_LateDenjuu_SubSubState], a
+    ret
+
+Victory_SubStateRecruitmentOpenNicknameScreen::
+    ld a, 1
+    call Banked_LCDC_PaletteFade
+    or a
+    ret z
+    ld a, [$D502]
+    ld [$C955], a
+    xor a
+    ld [W_LateDenjuu_SubSubState], a
+    ld [W_Battle_SubSubState], a
+    ld a, $1F
+    ld [W_SystemSubState], a
+    ld a, 3
+    ld [W_SystemState], a
     ret
