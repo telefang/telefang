9ac8e193977486fa30b5efc92c59a7d334f4944e
obskyr <powpowd@gmail.com>
2019-05-05 15:25:28 +0200

    Add font and name control code support to Sheets
    Along with a few miscellaneous improvements for formatting and preview.
    
    Answer control codes are for later - that'll be an endeavor in itself.

--- rip_scripts/sheets/preview.js
+++ rip_scripts/sheets/preview.js
@@ -1,23 +1,33 @@
-function previewSelectedCell() {
+function previewSelectedCells() {
   var sheet = SpreadsheetApp.getActiveSheet();
   var sheetName = sheet.getName();
   if (!SHEET_PARAMS.hasOwnProperty(sheetName)) {throw "This sheet isn't supported at the moment.";}
   var params = SHEET_PARAMS[sheetName];
   
-  var cell = sheet.getActiveCell();
-  var text = cell.getValue();
+  var ranges = sheet.getActiveRangeList().getRanges();
+  for (var i = 0; i < ranges.length; i++) {
+    var range = ranges[i];
+    for (var y = range.getLastRow(); y >= range.getRow(); y--) {
+      for (var x = range.getLastColumn(); x >= range.getColumn(); x--) {
+        var cell = sheet.getRange(y, x, 1, 1);
+        var text = cell.getValue();
+        
+        text = text.length > 0 ? text : " "; // REMOVE ONCE WEB PREVIEW HAS BEEN UPDATED
   
-  if (params.prelude && text.slice(0, params.prelude.length) === params.prelude) {
-    text = text.slice(params.prelude.length);
+        if (params.prelude && text.slice(0, params.prelude.length) === params.prelude) {
+          text = text.slice(params.prelude.length);
+        }
+        if (params.envoi && text.slice(text.length - params.envoi.length) === params.envoi) {
+          text = text.slice(0, text.length - params.envoi.length);
+        }
+        
+        if (cell.getColumn() == 4) { // Draft column (in the currently supported sheets, at least).
+          text = wrap(text, params.width, params.lines_per_prompt);
+        }
+        
+        var image = fetchImagePreview(text, params.width, params.line_spacing, params.lines_per_page, params.lines_per_prompt, params.min_lines);
+        sheet.insertImage(image, cell.getColumn(), cell.getRow(), sheet.getColumnWidth(cell.getColumn()) + 1, 0);
+      }
+    }
   }
-  if (params.envoi && text.slice(text.length - params.envoi.length) === params.envoi) {
-    text = text.slice(0, text.length - params.envoi.length);
-  }
-  
-  if (cell.getColumn() == 4) { // Draft column (in the currently supported sheets, at least).
-    text = wrap(text, params.width, params.lines_per_prompt);
-  }
-  
-  var image = fetchImagePreview(text, params.width, params.line_spacing, params.lines_per_page, params.lines_per_prompt, params.min_lines);
-  sheet.insertImage(image, cell.getColumn(), cell.getRow(), sheet.getColumnWidth(cell.getColumn()) + 1, 0);
 }
