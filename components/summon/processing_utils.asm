49c4e2fbfa72b70fd2f4e24c97f304706cbd6822
andwhyisit <realmmaster26@hotmail.com>
2020-12-30 15:22:28 +1000

    Optimised contact lookup times on the non-link summon screen.

--- components/summon/processing_utils.asm
+++ components/summon/processing_utils.asm
@@ -10,71 +10,89 @@ Summon_ParseContacts::
 	ld a, M_SaveClock_MaxDenjuuContacts
 	ld b, a
 	ld hl, W_PauseMenu_ContactsArray
+	ld de, $A000 + (M_SaveClock_DenjuuStatSize * M_SaveClock_MaxDenjuuContacts) + M_SaveClock_DenjuuLevel
+	ld c, M_SaveClock_DenjuuStatSize
+	call SaveClock_EnterSRAM2
 
 .loop
 	ld a, [W_PauseMenu_DeletedContact]
 	cp b
 	jr z, .skipContact
 
-	push hl
-	push bc
-	call SaveClock_EnterSRAM2
-	ld hl, $A001
-	ld a, b
-	call Battle_IndexStatisticsArray
-	ld a, [hl]
-	ld [W_Status_SelectedDenjuuLevel], a
-	pop bc
-	pop hl
-	ld a, [W_Status_SelectedDenjuuLevel]
-	cp 0
+	ld a, [de]
+	or a
 	jr z, .skipContact
 
+	ld [W_Status_SelectedDenjuuLevel], a
 	push hl
 	push bc
-	ld a, b
-	ld hl, $A008
-	call Battle_IndexStatisticsArray
-	ld a, [hl]
+	push de
+	ld a, e
+	add 7
+	ld e, a
+	ld a, [de]
 	ld b, a
 	ld a, [$D5A8]
 	call $05E3
-	ld a, [$D4EC]
+	pop de
 	pop bc
 	pop hl
+	ld a, [$D4EC]
 	cp 8
 	jr nc, .skipContact
 
 	push hl
-	ld hl, W_PhoneIME_DisplayedNumber
-	ld d, 0
+	ld h, W_PhoneIME_DisplayedNumber >> 8 ; This assumes that W_PhoneIME_DisplayedNumber will always be at 1:D200.
 	ld a, [W_Status_NumDuplicateDenjuu]
-	ld e, a
-	add hl, de
+	ld l, a
 	ld a, [$D4EC]
 	ld [hl], a
-	pop hl
-	push hl
-	ld hl, $D100
-	ld d, 0
-	ld a, [W_Status_NumDuplicateDenjuu]
-	ld e, a
-	add hl, de
+	ld h, $D1
 	ld a, [W_Status_SelectedDenjuuLevel]
 	ld [hl], a
+	ld a, l
+	inc a
+	ld [W_Status_NumDuplicateDenjuu], a
 	pop hl
 	ld a, b
 	ld [hli], a
-	ld a, [W_Status_NumDuplicateDenjuu]
-	inc a
-	ld [W_Status_NumDuplicateDenjuu], a
 
 .skipContact
-	ld a, 0
-	cp b
+	ld a, e
+	sub c
+	ld e, a
+	jr nc, .noDecD
+	dec d
+
+.noDecD
+	ld a, b
+	or a
 	jr z, .exit
 	dec b
 	jr .loop
 
 .exit
 	jp $3F12
+
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
