81b2536a2594315f5c8daf44649faf19a304aaf9
andwhyisit <realmmaster26@hotmail.com>
2022-11-10 23:17:11 +1000

    Support for rgbds v0.5.2.

--- components/saveclock/save_format.asm
+++ components/saveclock/save_format.asm
@@ -1,16 +1,19 @@
 INCLUDE "components/saveclock/save_format.inc"
 
 SECTION "Active Save File", SRAM[$A000], BANK[2]
-S_SaveClock_StatisticsArray:: REPT M_SaveClock_MaxDenjuuContacts
+S_SaveClock_StatisticsArray::
+	REPT M_SaveClock_MaxDenjuuContacts
 	ds M_SaveClock_DenjuuStatSize
 	ENDR
 	
 SECTION "Active Save File 2", SRAM[$B200], BANK[2]
-S_SaveClock_NicknameArray:: REPT M_SaveClock_MaxDenjuuContacts
+S_SaveClock_NicknameArray::
+	REPT M_SaveClock_MaxDenjuuContacts
 	ds M_SaveClock_DenjuuNicknameSize
 	ENDR
    
 SECTION "Active Save File 3", SRAM[$B000], BANK[1]
-S_SaveClock_InventoryArray:: REPT M_SaveClock_InventoryCount
+S_SaveClock_InventoryArray::
+	REPT M_SaveClock_InventoryCount
 	ds M_SaveClock_InventorySize
 	ENDR
